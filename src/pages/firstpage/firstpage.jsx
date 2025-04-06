a7b8ea016f35183c3e760e36c779d3ac5a87181b
Evelyn <crzycute4@gmail.com>
2025-04-06 17:50:46 +0800

    push

--- src/pages/firstpage/firstpage.jsx
+++ src/pages/firstpage/firstpage.jsx
@@ -4,7 +4,10 @@ import Profile from '../../assets/profilepro.png';
 import Uc from '../../assets/underconstruction.gif';
 import { useTypewriter, Cursor } from 'react-simple-typewriter';
 
-
+import fb from '../../assets/social-logo/facebook.png';
+import gmail from '../../assets/social-logo/gmail.png';
+import github from '../../assets/social-logo/github.png';
+import linkedin from '../../assets/social-logo/linkedin.png';
 
 function FirstPage() {
     const [text] = useTypewriter({
@@ -17,25 +20,36 @@ function FirstPage() {
     return (
         <div className="firstpage">
             <div className="left-icons">
-                {/* Your icon content here */}
+                <div className="social-icons-firstpage">
+                    <a href="https://facebook.com/yourprofile" target="_blank" rel="noopener noreferrer">
+                        <img src={fb} alt="Facebook" />
+                    </a>
+                    <a href="mailto:nikiskyacosta083@gmail.com">
+                        <img src={gmail} alt="Gmail" />
+                    </a>
+                    <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">
+                        <img src={github} alt="GitHub" />
+                    </a>
+                    <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">
+                        <img src={linkedin} alt="LinkedIn" />
+                    </a>
+                </div>
             </div>
-
-<div className="main-section">
-  <div className="intro-text">
-    <h1 className="name">Hi! I'm Nikki Sixx Acosta</h1>
-    <p className="typing-wrapper">
-      <span>{text}</span><Cursor cursorStyle='|' cursorColor='green' />
-    </p>
-<div className="bottom-image">
-  <img src={Uc} alt="Under Construction" style={{ width: '50%', height: 'auto' }} />
-</div>
-
-  </div>
-
-  <div className="profile-image">
-    <img src={Profile} alt="Profile" />
-  </div>
-</div>
+            <div className="main-section">
+                <div className="intro-text">
+                      <h1 className="name">Hi! I'm Nikki Sixx Acosta</h1>
+                          <p style={{fontWeight: 'bold'}}>I do</p>
+                          <p className="typing-wrapper">
+                            <span>{text}</span><Cursor cursorStyle='|' cursorColor='green' />
+                          </p>
+                    <div className="bottom-image">
+                      <img src={Uc} alt="Under Construction" style={{ width: '50%', height: 'auto' }} />
+                    </div>
+                </div>
+              <div className="profile-image">
+                  <img src={Profile} alt="Profile" />
+              </div>
+          </div>
 
         </div>
     );
